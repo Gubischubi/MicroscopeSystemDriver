d95179c169948dae81970a89c5bec843a2faae55
Alex <alexander.thees@gmx.de>
2018-07-31 15:03:00 +0200

    some new changes

---
+++ MicroscopeSystemDriver/MicroscopeDriver.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MicroscopeDriver
+{
+
+    public class Microscope
+    {
+
+        public double z0;
+
+    }
+
+    public class Table
+    {
+        public double x0;
+        public double y0;
+    }
+
+}
